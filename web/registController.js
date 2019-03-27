b91da64e8b977ed0141c2050f22bd4a61d545286
lorenzo marietta <lollomarj@hotmail.it>
2019-03-27 11:05:59 +0100

    cose minori

--- web/registController.js
+++ web/registController.js
@@ -4,9 +4,19 @@ var myApp = angular.module('regist', []).controller("registController",
 	$scope.checkPassword = function(){
             if($scope.password === $scope.passwordConfirm){
                 $scope.equal = "password corrispondenti";
+                $scope.colour = {'color':'green'};
             }else{
                 $scope.equal = "password non corrispondenti";
+                $scope.colour = {'color':'red'};
             }
         };
+        $scope.submit = function(){
+            if(window.regForm.account.value == "" || window.regForm.password.value == ""){
+                window.alert("compilare tutti i campi");
+                return;
+            }else{
+                window.regForm.submit();
+            }
+        } 
     });
 
