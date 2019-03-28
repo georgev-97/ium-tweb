c9b59180037d432d1a739470607dd64f7e5af251
lorenzo marietta <lollomarj@hotmail.it>
2019-03-28 16:08:13 +0100

    casino

--- web/courseProfessorController.js
+++ web/courseProfessorController.js
@@ -9,6 +9,22 @@ var myApp = angular.module('courseProfessor', []).controller("courseProfessorCon
                         $scope.professorList = response.data.professorList;
                     });
 
+            $scope.updateCourse = function () {
+                $scope.prof = $scope.professor.match(/\(.*\)/)[0]
+                        .replace(/\(/, "").replace(/\)/, "");
+                $http.get("/Ripetizioni/Controller", {params: {command: 'getFreeCourse', professor: $scope.prof}})
+                        .then(response => {
+                            if (response.data.error === "") {
+                                $scope.courseList = response.data.courseList;
+                            } else if (response.data.error === "no element") {
+                                $scope.courseList = response.data.courseList;
+                                window.alert("non è possibile associare il professore a nuovi corsi");
+                            } else {
+                                window.alert(response.data.error);
+                            }
+                        });
+            };
+
             $scope.submit = function () {
                 $scope.prof = $scope.professor.match(/\(.*\)/)[0]
                         .replace(/\(/, "").replace(/\)/, "");
@@ -18,8 +34,8 @@ var myApp = angular.module('courseProfessor', []).controller("courseProfessorCon
                             if (response.data.error === "") {
                                 window.alert("inserimento avvenuto");
                                 window.location.assign("admin.html");
-                            }else{
-                                window.alert("errore di inserimento");
+                            } else {
+                                window.alert(response.data.error);
                             }
                         });
             };
