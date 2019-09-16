c401cf5fec5a00719414a1e1025633b83eea2cec
georgev97 <georgeandrei.varga@gmail.com>
2019-09-16 09:33:52 +0200

    Lore

--- APP/TrovaRipetizioni/app/src/main/java/com/example/camelia/trovaripetizioni/Login.java
+++ APP/TrovaRipetizioni/app/src/main/java/com/example/camelia/trovaripetizioni/Login.java
@@ -2,12 +2,8 @@ package com.example.camelia.trovaripetizioni;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,9 +14,6 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.HttpClientStack;
-import com.android.volley.toolbox.HttpStack;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
